8be211e2389d9f149c2fb78cecc0c1d02a359b92
Mahhdy <mahhdy@gmail.com>
2020-05-02 19:13:29 -0400

    added back URL Config Static and Media

--- pizza/urls.py
+++ pizza/urls.py
@@ -30,4 +30,4 @@ urlpatterns = [
          name='password_reset_confirm',),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(),
          name='password_reset_complete',),
-]# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
