d1efbf6d935a704d63222c6a46ed72555c29900a
Mahhdy <mahhdy@gmail.com>
2020-05-02 18:23:52 -0400

    tried to fix static assets issue  for heroku

--- pizza/settings.py
+++ pizza/settings.py
@@ -148,12 +148,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/orders/static/'
 LOGIN_URL = 'login/'
-STATICFILES_DIRS = [
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/orders/static/'
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
-]
+)
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
